cdc98686ee23664cee3db9d3ba58b8b18c6a35c8
TDC28 <a06x03g07t01@protonmail.com>
2024-05-19 21:43:01 -0300

    Small fixes in install.sh

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ ranks the players by their performance throughout the year
 <img width="1551" alt="screenshot" src="https://github.com/TDC28/COCSheetBot/assets/89755772/38ad38cd-64ff-4ebb-acda-5e50e093bdff">
 
 # Installation
-$cd$ into your desired folder, then run this:
+`cd` into your desired folder, then run this:
 
 ```bash
 git clone https://github.com/TDC28/COCSheetBot.git
--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@ pip install -r requirements.txt
 echo '{"Players": {}}' >>playersList.json
 echo 'API_KEY=[Your COC api key goes here]' >>.env
 echo 'TOKEN=[The token required to use the bot goes here]' >>.env
-echo 'Installation was successful, copy Backup.xlsx into src and rename it. When using the !load_main command on discord you will use this filename'
-echo 'Open the .env file for final instructions'
-echo 'Once setup is finished, to run the but run the following from root of project:'
-echo 'source .venv/bin/activate'
-echo 'python src/main.py'
+
+printf '🎉 Installation was successful, copy Backup.xlsx into src and rename it. When using the !load_main command on discord you will use this filename'
+printf 'Open the .env file for final instructions'
+printf 'Once setup is finished, to start the bot, run the following from root of project:\n\n'
+printf '      source .venv/bin/activate'
+printf '      python src/main.py'
