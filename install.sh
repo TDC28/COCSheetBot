1607d6a066167a57069f0ad4616e4538464f2700
TDC28 <a06x03g07t01@protonmail.com>
2024-05-19 21:48:27 -0300

    Final tweaks to install

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@ pip install -r requirements.txt
 echo '{"Players": {}}' >>playersList.json
 echo 'API_KEY=[Your COC api key goes here]' >>.env
 echo 'TOKEN=[The token required to use the bot goes here]' >>.env
+deactivate
+rm install.sh
 
-printf '\n🎉 Installation was successful, copy Backup.xlsx into src and rename it. When using the !load_main command on discord you will use this filename\n'
-printf 'Open the .env file for final instructions\n'
-printf 'Once setup is finished, to start the bot, run the following from root of project:\n\n'
+printf '\n\n🎉 Installation was successful, copy Backup.xlsx into src and rename it. When using the !load_main command on discord you will use this filename.\n'
+printf '  Open the .env file for final instructions\n'
+printf '  Once setup is finished, to start the bot, run the following from root of project:\n\n'
 printf '      source .venv/bin/activate\n'
-printf '      python src/main.py'
+printf '      python src/main.py\n'
